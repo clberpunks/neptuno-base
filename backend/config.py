700de559284f3c7ef380d21c8c58ecdaed65f63b
vinxenx0 <vinxenxo@protonmail.com>
2025-06-26 01:27:12 +0200

    uri

--- backend/config.py
+++ backend/config.py
@@ -7,12 +7,12 @@ class Settings(BaseSettings):
     APP_NAME: str = "Ciberpunk"
     GOOGLE_CLIENT_ID: str
     GOOGLE_CLIENT_SECRET: str
-    GOOGLE_REDIRECT_URI: str = "http://localhost:8001/auth/callback"
+    GOOGLE_REDIRECT_URI: str = "https://ialert.ciberpunk.es/auth/callback"
     DATABASE_URL: str = "sqlite:///./app.db"
     CLIENT_SECRET: str = "your_client_secret_here"
     
     BACKEND_URL: str ="http://localhost:8001"
-    FRONTEND_URL: str ="http://localhost:3000"
+    FRONTEND_URL: str ="https://ialert.ciberpunk.es"
     
     # Nuevas variables para Refresh Token
     REFRESH_SECRET: str = "super_refresh_secret"
